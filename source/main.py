7c7d59179e9610ebfc04e71496318109d8aa2528
zack2439 <baileyzack321@gmail.com>
2026-07-20 15:34:19 -0400

    added er diagram, schema, current db file, sql queries that have been run so far, some main implementation

--- source/main.py
+++ source/main.py
@@ -1,9 +1,39 @@
+import sqlite3
+# Queries
+GET_ALL_WAREHOUSES = '''select * from warehouse'''
+GET_ALL_PRODUCTS = '''select * from product'''
+GET_ALL_PARTS = '''select * from part'''
+GET_ALL_EMPLOYEES = '''select * from employee'''
 
 
+# Create connection to db
+connection = sqlite3.connect('inventory.db')
+cur = connection.cursor()
+
+
+# Helper
+def print_table(data, attr_names):
+    if not data:
+        print("Empty table - error.")
+    # Headers
+    for attr in attr_names:
+        print(attr, end='    ')
+
+    print('\n')
+    # Columns
+    for row in data:
+        for value in row:
+            print(value, end='  |  ')
+        print('\n')
+
+def get_attr_names(cursor):
+    return [description[0] for description in cursor.description]
+
+# Interface
 def interface():
     print("Welcome to the inventory system.")
-    print("1. Add an item")
-    print("2. View all items")
+    print("1. Add a product")
+    print("2. View all products")
     print("3. View Employees")
     print("4. Quit")
 
@@ -13,18 +43,30 @@ def interface():
         if command.lower() == '1':
             # Add item to the inventory with insert into cmd,
             # item must have name, cost, quantity, warehouse, and parts
-            print("Please enter the item details:")
+            print("Please enter the product details:")
+            # Get attrs for product
             name = input("Name: ")
             cost = float(input("Cost: "))
             quantity = int(input("Quantity: "))
-            warehouse = input("Warehouse: ")
-            print("Below is a list of parts, select parts by id associated with the item:")
+            print("Select a warehouse from the list below:")
+
+            # Printing out warehouses to get foreign key for product
+            res = cur.execute(GET_ALL_WAREHOUSES)
+            print_table(res.fetchall(), get_attr_names(cur))
             
+            warehouse = input("Warehouse id: ")
+
+            # Printing out parts because each product must have parts, 
+            # this relationship will be inserted into product/part rel table
+            print("Below is a list of parts, select parts by id associated with the product:")
+            res = cur.execute(GET_ALL_PARTS)
+            print_table(res.fetchall(), get_attr_names(cur))
+
             pass
 
         elif command.lower() == '2':
-            # View all items in the inventory, 
-            # this will next prompt user to view parts or warehouse of chosen item
+            # View all products in the inventory, 
+            # this will next prompt user to view parts or warehouse of chosen product
             pass
 
         elif command.lower() == '3':
