4ac465772173d71bebb6eef984176d47f35937b7
zack2439 <baileyzack321@gmail.com>
2026-07-23 21:03:41 -0400

    more special queries, added product deletion

--- source/main.py
+++ source/main.py
@@ -1,22 +1,42 @@
 import sqlite3
 from pathlib import Path
 from tabulate import tabulate
-# Queries
+# Static Queries
 GET_ALL_WAREHOUSES = '''select * from warehouse'''
 GET_ALL_PRODUCTS = '''select * from product'''
 GET_ALL_PARTS = '''select * from part'''
 GET_ALL_EMPLOYEES = '''select * from employee'''
 GET_EMPLOYEES_WITH_GREATER_AVG_SALARY = '''select * from employee where salary > 
                                             (select avg(salary) from employee)'''
+
+GET_EMPLOYEES_UNDER_100000_SAL_WITH_WAREHOUSE = """select e.employee_id, e.name as employee_name, 
+                                                    w.name as warehouse_name, w.state, w.city 
+                                                    from employee as e
+                                                    inner join warehouse as w
+                                                    on e.warehouse_id = w.warehouse_id
+                                                    where e.salary < 100000;"""
+
+GET_PRODUCTS_FROM_WAREHOUSE_WITH_MORE_THAN_ONE_PRODUCT = """select p.name as product_name, p.quantity, p.cost, w.name as warehouse_name, w.state, w.city
+                                                            from product as p
+                                                            inner join warehouse as w
+                                                            on p.warehouse_id = w.warehouse_id
+                                                            where p.warehouse_id in (select warehouse_id
+                                                                                    from product
+                                                                                    group by warehouse_id
+                                                                                    having count(warehouse_id)>1)
+                                                            order by w.name;"""
+
 db_dir = str(Path(__file__).resolve().parent.parent) + '/inventory.db'
 # TODO: Query that shows products that are being handled by a particular employee
 # TODO: Code cleanup, make print table more readable
 # TODO: Add product insertion and more queries
+# TODO: Warehouses with more than one product, and the products in those warehouses
 
 # Create connection to db
 connection = sqlite3.connect(db_dir)
 cur = connection.cursor()
-
+# Turn on foreign key constraint
+connection.execute("PRAGMA foreign_keys = ON;")
 
 # Helper
 def print_table(data, attr_names):
@@ -33,8 +53,8 @@ def interface():
 
         print("Welcome to the inventory system.")
         print("1. Add a product")
-        print("2. View all products")
-        print("3. View Employees")
+        print("2. Product view/deletion")
+        print("3. Employee view")
         print("4. Special Queries")
         print("5. Quit\n")
         command = input("Enter your choice (1-5): ")
@@ -75,7 +95,7 @@ def interface():
             parts = input("Enter part ids separated by commas: ")
             parts = [int(x.strip()) for x in parts.split(',')]
             # Make sure product is being added with parts
-            print('asd')
+            
             if not parts:
                 print("You must select at least one part.")
                 continue
@@ -96,10 +116,11 @@ def interface():
             res = cur.execute(GET_ALL_PRODUCTS)
             print_table(res.fetchall(), get_attr_names(cur))
 
-            chosen_product = int(input("Select a product by id to view its details: "))
+            chosen_product = int(input("Select a product by id to view details or delete: "))
             print("1. View parts associated with this product")
             print("2. View warehouse associated with this product")
-            choice = input("Enter your choice (1-2): ")
+            print("3. Delete this product from the database")
+            choice = input("Enter your choice (1-3): ")
             # Selecting parts
             if choice == '1':
                 res = cur.execute(f"select * from part where part_id in (select part_id from parts_in_product where product_id = {chosen_product})")
@@ -110,6 +131,24 @@ def interface():
                 res = cur.execute(f"select * from warehouse where warehouse_id in (select warehouse_id from product where product_id = {chosen_product})")
                 print_table(res.fetchall(), get_attr_names(cur))
 
+            elif choice == '3':
+                print(chosen_product)
+                # Delete product from parts_in_product rel table
+                try:
+                    cur.execute(f"delete from parts_in_product where product_id = {chosen_product};")
+                    connection.commit()
+                except:
+                    print("Error deleting product from parts_in_product table")
+                    continue
+                # Delete product from database
+                try:
+                    cur.execute(f"delete from product where product_id = {chosen_product};")
+                    connection.commit()
+                    print("Product deleted successfully.")
+                except:
+                    print("Error deleting product")
+                    continue
+                
 
         elif command.lower() == '3':
             # View employee information
@@ -126,13 +165,19 @@ def interface():
             # Special Queries
             print("Special Queries:")
             print("1. View employees with salary greater than average")
-
+            print("2. View employees with salary under 100000 and their warehouse details")
+            print("3. View products from warehouse with more than one product")
             special_query = input("Enter your choice (1-3): ")
 
             if special_query == '1':
                 res = cur.execute(GET_EMPLOYEES_WITH_GREATER_AVG_SALARY)
                 print_table(res.fetchall(), get_attr_names(cur))
-            
+            elif special_query == '2':
+                res = cur.execute(GET_EMPLOYEES_UNDER_100000_SAL_WITH_WAREHOUSE)
+                print_table(res.fetchall(), get_attr_names(cur))
+            elif special_query == '3':
+                res = cur.execute(GET_PRODUCTS_FROM_WAREHOUSE_WITH_MORE_THAN_ONE_PRODUCT)
+                print_table(res.fetchall(), get_attr_names(cur))
             
 
         elif command.lower() == '5':
